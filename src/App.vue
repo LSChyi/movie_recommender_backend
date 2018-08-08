b1bca8f1cdb736820e269c6e049880718b7d68d2
LSChyi <alan81920@gmail.com>
2018-08-08 19:21:34 +0800

    build basic index page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <b-navbar toggleable="md" type="light" variant="light">
+      <b-navbar-toggle target="nav_text_collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#">Movie Recommender</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item>
+            <a href="#">Sign in</a> or <a href="#">Sign up</a>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <br />
     <router-view/>
   </div>
 </template>
@@ -17,7 +28,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Movie Recommender System</h1>
+    <h2>Based on 10M Movielens Dataset</h2>
+    <ul>
+      <li>
+        <a href="">Sign in</a>
+      </li>
+      <li>
+        <a href="">Sign up</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
