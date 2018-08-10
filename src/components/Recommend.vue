6cf69f748fbb5510c961879607a9615635612559
LSChyi <alan81920@gmail.com>
2018-08-10 14:37:49 +0800

    add basic recommended movie page

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
-    this is recommand page
+    <b-container>
+      <h2>Recommended Movies</h2>
+      <b-card-group deck>
+        <movie v-for="movie in movies" :key="movie.id" :movie="movie"></movie>
+      </b-card-group>
+    </b-container>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Movie from '@/components/movie'
 
 export default {
+  created: function () {
+    this.get_recommendations()
+  },
+  components: {
+    Movie
+  },
+  data: function () {
+    return {
+      movies: []
+    }
+  },
   methods: {
+    get_recommendations () {
+      axios.get('/movies/recommendations/')
+        .then((res) => {
+          this.movies = res.data
+        })
+    }
   }
 }
 </script>
---
+++ src/components/movie.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-card
+      :title="movie.name"
+      :img-src="'https://image.tmdb.org/t/p/w780' + movie.poster_partial_url"
+      img-alt="Image"
+      img-top
+      class="mb-2"
+      bg-variant="dark"
+      border-variant="white"
+      style="max-width: 20rem;"
+      >
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'movie' ]
+}
+</script>
