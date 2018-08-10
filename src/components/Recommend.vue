4d60d24bc654cbddd9d2559dcde738346875279c
LSChyi <alan81920@gmail.com>
2018-08-10 22:42:23 +0800

    add automatically update recommendation list

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -2,6 +2,10 @@
   <div>
     <b-container>
       <h2>Recommended Movies</h2>
+      <div style="margin:12px">
+        <b-progress :value="countdown.now" :max="countdown.max" animated></b-progress>
+        <div>The recommendation list will be updated in {{ countdown_seconds }} seconds</div>
+      </div>
       <b-card-group deck>
         <movie v-for="movie in movies" :key="movie.id" :movie="movie"></movie>
       </b-card-group>
@@ -16,21 +20,47 @@ import Movie from '@/components/movie'
 export default {
   created: function () {
     this.get_recommendations()
+      .then(() => this.start_countdown())
   },
   components: {
     Movie
   },
+  computed: {
+    countdown_seconds () {
+      return Math.ceil(this.countdown.now)
+    }
+  },
   data: function () {
     return {
-      movies: []
+      movies: [],
+      countdown: {
+        max: 6,
+        now: 6
+      }
     }
   },
   methods: {
     get_recommendations () {
-      axios.get('/movies/recommendations/')
-        .then((res) => {
-          this.movies = res.data
-        })
+      return new Promise((resolve, reject) => {
+        axios.get('/movies/recommendations/')
+          .then((res) => {
+            this.movies = res.data
+            resolve()
+          })
+      })
+    },
+    start_countdown () {
+      this.countdown.now = this.countdown.max
+      this.count_minus()
+    },
+    count_minus () {
+      if (this.countdown.now > 0.1) {
+        this.countdown.now -= 0.1
+        setTimeout(this.count_minus, 100)
+      } else {
+        this.get_recommendations()
+          .then(() => this.start_countdown())
+      }
     }
   }
 }
